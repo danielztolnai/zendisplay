63727b21535898ce5a8a422225b7d6b433a9315d
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2022-09-03 07:51:22 +0200

    Move configuration options to a configuration file

--- zendisplay.py
+++ zendisplay.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Small script to adjust display brightness according to ambient lighting"""
+import configparser
 import os
 import sys
 from PyQt5 import QtWidgets
@@ -14,20 +15,61 @@ from luminance_iio import LuminanceIIO
 from luminance_manual import LuminanceManual
 from luminance_mqtt import LuminanceMQTT
 
-DEFAULT_SENSOR = 0
-SHOW_NOTIFICATIONS = False
-MQTT_HOST = None
-MQTT_TOPIC = None
-MQTT_PUBLISH = False
+class Config(configparser.ConfigParser): # pylint: disable=too-many-ancestors
+    """Manage configuration files"""
+    _instance = None
+    _initialized = False
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        if self._initialized:
+            return
+        super().__init__()
+        self._load_config_file()
+        self._initialized = True
+
+    @staticmethod
+    def _get_config_file_paths():
+        """Generate config path priority list"""
+        return (
+            os.path.join(
+                os.environ.get('XDG_CONFIG_HOME', os.path.expanduser('~/.config')),
+                'zendisplay',
+                'zendisplay.conf'
+            ),
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zendisplay.conf'),
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zendisplay.conf.example'),
+        )
+
+    def _load_config_file(self):
+        """Read configuration"""
+        for path in self._get_config_file_paths():
+            if os.path.isfile(path):
+                self.read(path)
+                return
+
+    def save(self):
+        """Write current data to configuration file"""
+        path = self._get_config_file_paths()[0]
+        try:
+            if not os.path.exists(os.path.dirname(path)):
+                os.mkdir(os.path.dirname(path), mode=0o755)
+            with open(path, 'w') as config_file:
+                self.write(config_file)
+        except (FileNotFoundError, OSError) as exception:
+            print('Could not save configuration: {}'.format(exception))
 
 class Controller:
     """Recommends new brightness value based on current data"""
-    BRIGHTNESS_INCREMENT = 5
-
     def __init__(self):
-        self.brightness_margin = 5
-        self.line_m = 0.2
-        self.line_b = 0
+        self.brightness_increment = Config().getint('brightness', 'increment', fallback=5)
+        self.brightness_margin = Config().getint('brightness', 'margin', fallback=5)
+        self.line_m = Config().getfloat('brightness', 'slope', fallback=0.2)
+        self.line_b = Config().getint('brightness', 'base_value', fallback=0)
 
     def calculate_brightness(self, luminance):
         """Calculate brightness from ambient lighting"""
@@ -62,15 +104,16 @@ class Controller:
     def set_intercept(self, value):
         """Set the slope of the brightness function"""
         self.line_b = value
+        Config().set('brightness', 'base_value', str(value))
 
     def increase_intercept(self):
         """Increase brightness function intercept"""
-        self.line_b = min(self.line_b + self.BRIGHTNESS_INCREMENT, 100)
+        self.set_intercept(min(self.line_b + self.brightness_increment, 100))
         return self.line_b
 
     def decrease_intercept(self):
         """Decrease brightness function intercept"""
-        self.line_b = max(self.line_b - self.BRIGHTNESS_INCREMENT, 0)
+        self.set_intercept(max(self.line_b - self.brightness_increment, 0))
         return self.line_b
 
 
@@ -92,13 +135,18 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
         self.sensors = LuminanceSourceManager()
         self.sensors.add_source_type(LuminanceDBus)
         self.sensors.add_source_type(LuminanceIIO)
-        self.sensors.add_source_type(LuminanceMQTT, {'topic': MQTT_TOPIC, 'host': MQTT_HOST})
+        if Config().getboolean('mqtt', 'subscribe') is True:
+            self.sensors.add_source_type(LuminanceMQTT, {
+                'topic': Config().get('mqtt', 'topic'),
+                'host': Config().get('mqtt', 'host'),
+            })
+
         self.sensors.add_source_type(LuminanceManual, {
             'cb_enable': lambda: self.controller.set_intercept(self.displays.get_brightness()),
             'cb_disable': self.controller.set_intercept,
             'get_value': lambda: self.controller.line_b,
         })
-        self.sensors.activate(DEFAULT_SENSOR)
+        self.sensors.activate(Config().getint('general', 'default_sensor', fallback=0))
 
         self.menu = self.construct_menu()
         self.menu_visible = False
@@ -109,8 +157,12 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
         self.timer.timeout.connect(self.main_control)
         self.timer.start(1000)
 
-        if MQTT_PUBLISH is True:
-            self.mqtt_publisher = LuminanceMQTT(name="mqttp", path=MQTT_TOPIC, host=MQTT_HOST)
+        if Config().getboolean('mqtt', 'publish') is True:
+            self.mqtt_publisher = LuminanceMQTT(
+                name="mqttp",
+                path=Config().get('mqtt', 'topic'),
+                host=Config().get('mqtt', 'host'),
+            )
 
     def toggle_menu(self):
         """Toggle context menu visibility"""
@@ -129,6 +181,8 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
 
         # Create quit button
         menu.addSeparator()
+        action_save = menu.addAction("Save settings")
+        action_save.triggered.connect(lambda _: Config().save())
         action_quit = menu.addAction("Quit")
         action_quit.triggered.connect(lambda _: sys.exit())
         return menu
@@ -163,7 +217,7 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
         for value in range(0, 101, 10):
             action = brightness_menu.addAction(str(value) + "%")
             action.setCheckable(True)
-            if value == 0:
+            if value == Config().getint('brightness', 'base_value', fallback=0):
                 action.setChecked(True)
             action.triggered.connect(lambda _, value=value: self.controller.set_intercept(value))
             brightness_group.addAction(action)
@@ -182,7 +236,7 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
 
         self.setToolTip('Brightness: ' + str(recommended_brightness) + '%')
         self.displays.set_brightness(recommended_brightness)
-        if MQTT_PUBLISH is True:
+        if Config().getboolean('mqtt', 'publish') is True:
             self.mqtt_publisher.publish(luminance)
 
     def event(self, event):
@@ -193,7 +247,8 @@ class ZenDisplay(QtWidgets.QSystemTrayIcon):
                 new_value = self.controller.decrease_intercept()
             else:
                 new_value = self.controller.increase_intercept()
-            if new_value is not None and self.supportsMessages() and SHOW_NOTIFICATIONS:
+            show_notification = Config().getboolean('general', 'show_notifications')
+            if new_value is not None and self.supportsMessages() and show_notification:
                 self.showMessage('Brightness', str(new_value) + '%', msecs=500)
             return True
         return False
