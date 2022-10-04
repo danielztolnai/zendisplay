28d1ae9bacd10bef7094e976f62228533efeb187
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2022-10-04 12:46:31 +0200

    Support display mode in mqtt module

--- luminance_mqtt.py
+++ luminance_mqtt.py
@@ -1,27 +1,29 @@
 """Get or provide ambient lighting data through mqtt"""
 import paho.mqtt.client as mqtt
+from zendisplay_config import Config
 from base_classes import LuminanceSource, Display
 
-class LuminanceMQTT(LuminanceSource):
+class LuminanceMQTT(LuminanceSource, Display):
     """Get ambient lighting information from MQTT"""
     PARAMETER_MQTT_HOST = 'host'
     PARAMETER_MQTT_TOPIC = 'topic'
 
     def __init__(self, name=None, path=None, host=None):
         super().__init__(name, path)
+        self.enabled = False
         self.luminance = 0
+        self.mqtt_host = host
         # Create MQTT client
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.on_disconnect = self.on_disconnect
-        self.client.connect(host, 1883, 60)
 
     @classmethod
     def detect(cls, parameters):
         """Add given MQTT configuration"""
-        topic = parameters[cls.PARAMETER_MQTT_TOPIC]
-        host = parameters[cls.PARAMETER_MQTT_HOST]
+        topic = Config().get('mqtt', 'topic')
+        host = Config().get('mqtt', 'host')
         if host is None:
             return
         yield cls(name="mqtt", path=topic, host=host)
@@ -33,12 +35,14 @@ class LuminanceMQTT(LuminanceSource):
     def enable(self):
         """Enable the source"""
         super().enable()
+        self.client.connect(self.mqtt_host, 1883, 60)
         self.client.loop_start()
 
     def disable(self):
         """Disable the source"""
         super().disable()
         self.client.loop_stop(force=False)
+        self.client.disconnect()
 
     def on_connect(self, _1, _2, _3, _4):
         """Called when connection is established to the MQTT server"""
@@ -54,6 +58,20 @@ class LuminanceMQTT(LuminanceSource):
         if reason_code is not mqtt.MQTT_ERR_SUCCESS:
             self._set_ready(False)
 
-    def publish(self, value):
+    def get_brightness(self):
+        """Get current brightness of the display"""
+        if self.enabled:
+            return self.luminance
+        return None
+
+    def set_brightness(self, brightness):
+        """Set brightness of the display"""
+        if brightness == self.get_brightness() or brightness < 0:
+            return
+
+        if self.enabled:
+            self._set_brightness(brightness)
+
+    def _set_brightness(self, brightness):
         """Publish given data to the topic of the object"""
-        self.client.publish(self.path, value)
+        self.client.publish(self.path, brightness)
