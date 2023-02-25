4d6ec6b4d43263290fb66d9e346ab5ff863edf35
Dániel Zoltán Tolnai <danielz.tolnai@gmail.com>
2023-02-25 10:21:04 +0100

    Add delay to iio-sensor-proxy source
    For some sensors, iio-sensor-proxy returns 0 brightness early after it appears.
    This delay makes sure that the sensor is stable before reading the first value.

--- luminance_dbus.py
+++ luminance_dbus.py
@@ -1,4 +1,5 @@
 """Get ambient lighting data through dbus from iio-sensor-proxy"""
+from threading import Timer
 import dbus
 from dbus_object import DBusObject
 from base_classes import LuminanceSource
@@ -9,6 +10,7 @@ class LuminanceDBus(LuminanceSource):
         super().__init__(name, path)
         self.luminance = 0
         self.luminance_prop = 'LightLevel'
+        self.appear_timer = None
 
         # Configure DBus connection
         self.bus = dbus.SystemBus()
@@ -32,12 +34,20 @@ class LuminanceDBus(LuminanceSource):
         print(f'{self.name} appeared')
         self.sensor.watch_properties(self.handle_sensor_proxy_signal)
         self.sensor.interface().ClaimLight()
+        self.appear_timer = Timer(1.0, self.sensor_read)
+        self.appear_timer.start()
+
+    def sensor_read(self):
+        """Read sensor manually"""
         self.luminance = self.sensor.get_property(self.luminance_prop)
         self._set_ready(True)
+        print(f'{self.name} ready')
 
     def sensor_disconnect(self):
         """Detach sensor"""
         print(f'{self.name} vanished')
+        if self.appear_timer:
+            self.appear_timer.cancel()
         self._set_ready(False)
         self.sensor.watch_properties_stop()
 
